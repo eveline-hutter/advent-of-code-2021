ea391c81ab11e66f95e0e6443f048a640314a9ca
eveline-hutter <eveline.hutter@students.ffhs.ch>
2021-12-03 08:50:59 +0100

    refactor day1

--- day1/main.py
+++ day1/main.py
@@ -1,11 +1,13 @@
 with open('input1.txt') as file:
     input = [int(line) for line in file]
 
+
 def puzzle1():
     increases = 0
     for i in range(len(input) - 1):
         if input[i] < input[i + 1]: increases += 1
     print(increases)
+
 
 def puzzle2():
     increases = 0
@@ -14,6 +16,7 @@ def puzzle2():
         sum2 = sum(input[i+1:i+4])
         if sum2 > sum1: increases += 1
     print(increases)
+
 
 puzzle1()
 puzzle2()
